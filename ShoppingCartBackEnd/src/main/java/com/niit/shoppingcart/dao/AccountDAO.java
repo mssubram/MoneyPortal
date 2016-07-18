64ccb0909eee8e5e78ddc88d933d53f6b1b5fbf3
mssubram <msubram3@ncsu.edu>
2016-07-18 22:27:31 +0530

    Revert "Revert "Front and Backend""
    This reverts commit 75036f91a3fbdba78fd05affc3ceec9990a83c03.

---
+++ ShoppingCartBackEnd/src/main/java/com/niit/shoppingcart/dao/AccountDAO.java
@@ -0,0 +1,10 @@
+package com.niit.shoppingcart.dao;
+
+import com.niit.shoppingcart.model.Account;
+
+public interface AccountDAO {
+	public Account get(String id);
+	public boolean trasfer(String id, int amount);
+
+
+}
